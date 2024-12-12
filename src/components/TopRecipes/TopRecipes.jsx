f2cc5ba929ffa101fb81a989a465414fcf59899a
unknown <soundouce.chibani@gmail.com>
2024-12-12 14:40:07 +0100

    updated CSS class names by adding -componentName to prevent conflicts and improved visual styling

--- src/components/TopRecipes/TopRecipes.jsx
+++ src/components/TopRecipes/TopRecipes.jsx
@@ -4,39 +4,42 @@ import './TopRecipes.css'
 
 export default function TopRecipes() {
   return (
-    <div className='group'>
-      <div className='card'>
+    <div className='container'>
+      <h2>Top recettes</h2>
+    <div className='card-container'>
+      <div className='card-topRecipes'>
         <img src={recipeImage}/>
-        <div className='description'>
-          <h3 className='cardTitle'>Titre de la recette</h3>
-          <p className='detail'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
+        <div className='description-topRecipes'>
+          <h3 className='cardTitle-topRecipes'>Titre de la recette</h3>
+          <p className='detail-topRecipes'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
           <p className='text-muted'>Aujourd'hui</p>
         </div>
       </div>
-      <div className='card'>
+      <div className='card-topRecipes'>
         <img src={recipeImage}/>
-        <div className='description'>
-          <h3 className='cardTitle'>Titre de la recette</h3>
-          <p className='detail'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
+        <div className='description-topRecipes'>
+          <h3 className='cardTitle-topRecipes'>Titre de la recette</h3>
+          <p className='detail-topRecipes'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
           <p className='text-muted'>Aujourd'hui</p>
         </div>
       </div>
-      <div className='card'>
+      <div className='card-topRecipes'>
         <img src={recipeImage}/>
-        <div className='description'>
-          <h3 className='cardTitle'>Titre de la recette</h3>
-          <p className='detail'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
+        <div className='description-topRecipes'>
+          <h3 className='cardTitle-topRecipes'>Titre de la recette</h3>
+          <p className='detail-topRecipes'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
           <p className='text-muted'>Aujourd'hui</p>
         </div>
       </div>
-      <div className='card'>
+      <div className='card-topRecipes'>
         <img src={recipeImage}/>
-        <div className='description'>
-          <h3 className='cardTitle'>Titre de la recette</h3>
-          <p className='detail'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
+        <div className='description-topRecipes'>
+          <h3 className='cardTitle-topRecipes'>Titre de la recette</h3>
+          <p className='detail-topRecipes'>Ajoutez ces graines riches en oméga-3 dans votre yaourt régulièrement selon cette nutritionniste</p>
           <p className='text-muted'>Aujourd'hui</p>
         </div>
       </div>
+    </div>
     </div>
   )
 }
