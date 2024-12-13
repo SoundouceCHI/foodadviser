feb8995b0b079ee3dc4cda46f07c3a97a8d543c5
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2024-12-13 18:13:51 +0100

    Edit ingredients component

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -2,42 +2,40 @@ import { FaMinus } from "react-icons/fa";
 import orange from "../../assets/orange.jpg";
 import "./Ingredients.css";
 
+
 export default function Ingredients() {
+    const ingredients = [
+        { id: 1, title: "Orange", image: orange },
+        { id: 2, title: "Orange", image: orange },
+        { id: 3, title: "Orange", image: orange },
+        { id: 4, title: "Orange", image: orange },
+        { id: 1, title: "Orange", image: orange },
+        { id: 2, title: "Orange", image: orange },
+        { id: 3, title: "Orange", image: orange },
+        { id: 4, title: "Orange", image: orange },
+      ];
+      const groupedIngredients = [];
+      for (let i = 0; i < ingredients.length; i += 3) {
+        groupedIngredients.push(ingredients.slice(i, i + 3));
+      }
   return (
     <>
         <div className="container">
             <h2 className="title-ingredients">Vous avez comme ingrédients :</h2>
-            <div className="card-ingredient-container">
-                <div className="card-ingredient">
-                    <img src={orange} alt="" className="card-image"/>
+            {groupedIngredients.map((group, index) => (
+        <div key={index} style={{ display: "flex", gap: "20px" }}>
+          {group.map((ingredient, idx) => (
+                <div className="card-ingredient" key={`${index}-${idx}`}>
+                    <img src={ingredient.image} alt="" className="card-image"/>
                     <div className="card-ingredient-content">
-                        <h3>Orange</h3>
+                        <h3>{ingredient.title}</h3>
                         <button className="ingredient-btn"><FaMinus /></button>
                     </div>
                 </div> 
-                <div className="card-ingredient">
-                    <img src={orange} alt="" className="card-image"/>
-                    <div className="card-content">
-                        <h3>Orange</h3>
-                        <button className="ingredient-btn"><FaMinus /></button>
-                    </div>
-                </div> 
-                <div className="card-ingredient">
-                    <img src={orange} alt="" className="card-image"/>
-                    <div className="card-content">
-                        <h3>Orange</h3>
-                        <button className="ingredient-btn"><FaMinus /></button>
-                    </div>
-                </div> 
-                <div className="card-ingredient">
-                    <img src={orange} alt="" className="card-image"/>
-                    <div className="card-content">
-                        <h3>Orange</h3>
-                        <button className="ingredient-btn"><FaMinus /></button>
-                    </div>
-                </div> 
+                ))}
+                </div>
+              ))}
             </div>
-        </div>
     </>
   )
 }
