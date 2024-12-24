d6565b16e04b4132d8656b2d0b351a16c2c78b62
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2024-12-24 16:19:44 +0100

    Updated Recipes and DetailRecipe and Ingredients to fetch recipes and their details from the database

--- src/pages/DetailRecipe.jsx
+++ src/pages/DetailRecipe.jsx
@@ -1,31 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRecipeById } from "../services/api";
-import Ingredients from "../components/Ingredients/Ingredients.jsx"; 
-import RecipeSteps from "../components/RecipeSteps/RecipeSteps.jsx"; 
+import Ingredients from "../components/Ingredients/Ingredients.jsx";
+import RecipeSteps from "../components/RecipeSteps/RecipeSteps.jsx";
 import "../styles/DetailRecipe.css";
 
 export default function DetailRecipe() {
-  const { recipeId } = useParams(); 
+  const { recipeId } = useParams();
   const [recipeDetails, setRecipeDetails] = useState(null);
   const [error, setError] = useState(null);
 
-  // Charger les détails de la recette
-  const fetchRecipeDetails = async () => {
-    try {
-      const data = await getRecipeById(recipeId);
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setRecipeDetails(data);
-      }
-    } catch (err) {
-      console.error('Error fetching recipe:', err.message);
-      setError("Erreur de connexion au serveur");
-    }
-  };
-
   useEffect(() => {
+    const fetchRecipeDetails = async () => {
+      try {
+        const data = await getRecipeById(recipeId);
+        if (data.error) {
+          setError(data.error);
+        } else {
+          setRecipeDetails(data);
+        }
+      } catch (err) {
+        console.error("Error fetching recipe:", err.message);
+        setError("Erreur de connexion au serveur");
+      }
+    };
+
     fetchRecipeDetails();
   }, [recipeId]);
 
@@ -37,7 +36,6 @@ export default function DetailRecipe() {
     return <p>Chargement des détails de la recette...</p>;
   }
 
-  // Extraire les ingrédients et les étapes depuis les détails de la recette
   const ingredients = recipeDetails.ingredients || [];
   const steps = recipeDetails.steps || [];
 
