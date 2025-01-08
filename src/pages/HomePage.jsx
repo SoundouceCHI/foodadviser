ba30167863c0f4a82869126bb54479c9beb80328
Aya Benmakhlouf <benmakhloufaya1@gmail.com>
2025-01-08 23:16:05 +0100

    Add SearchBar and ImageManager components to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Recipes from "../components/Recipes/Recipes";
 import TopRecipes from "../components/TopRecipes/TopRecipes";
 import SearchBar from "../components/SearchBar/SearchBar";
+import ImageManager from "../components/ImageManager/ImageManager";
+import '../styles/HomePage.css'
 
 export default function HomePage() {
   return (
     <>
-      <SearchBar/>
+      <div className="aabody">
+        <SearchBar/>
+        <ImageManager/>
+      </div>
       <Recipes limit={4} random={true} />
       <TopRecipes />
     </>
